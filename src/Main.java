5b6ce51befaac39b98c604812849a97ab575ba7a
misharka <fareshta88@gmail.com>
2020-11-14 19:29:33 +0300

    main.java and report.md added

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int CurrentAccountBalance = 2_000_000_000;
+        int TransferAmount = 500_000_000;
+        int TotalAccountBalance = CurrentAccountBalance + TransferAmount;
+        System.out.println("Total Account Balance:");
+        System.out.println(TotalAccountBalance);
+        System.out.println("Done");
+    }
+}
